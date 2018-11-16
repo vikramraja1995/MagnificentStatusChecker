0d4357573194703e3b3e16b8ed3eb97b1ce0b084
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-11-16 14:14:43 -0800

    Set up dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
