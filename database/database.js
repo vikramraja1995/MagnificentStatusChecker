1eae4bccfaef513c504422fd8d6e82b78fdf9280
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-11-16 15:36:45 -0800

    Set up schema, model and addStatus function

--- database/database.js
+++ database/database.js
@@ -9,3 +9,37 @@ const db = mongoose.connection;
 mongoose.Promise = Promise;
 // eslint-disable-next-line no-console
 db.on('error', e => console.error('Connection Error:', e));
+/* --------------------------------------------------------------------------------------------- */
+
+// Set up Schema and Model
+const statusHistorySchema = new mongoose.Schema(
+  {
+    website: String, // Which website's status is being saved
+    timestamp: String,
+    code: Number,
+    response: String,
+  },
+  // Strict is set to false as response is only recorded if a non 200 code is received
+  { strict: false },
+);
+
+const StatusHistory = mongoose.model('StatusHistory', statusHistorySchema);
+/* --------------------------------------------------------------------------------------------- */
+
+// DB Querying Functions
+const addStatus = (website, timestamp, code, response = '') => {
+  const data = {
+    website,
+    timestamp,
+    code,
+  };
+  if (response.length > 0) {
+    data.response = response;
+  }
+  const status = new StatusHistory(data);
+  return status.save();
+};
+
+/* --------------------------------------------------------------------------------------------- */
+
+module.exports = { addStatus };
