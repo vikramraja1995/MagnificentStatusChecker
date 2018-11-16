9c441ae12fa0d0e818ba68537d8aa2596606815d
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-11-16 14:45:19 -0800

    Fix webpack config formatting to match airbnb style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  entry: __dirname + '/client/src/index.jsx',
+  entry: `${__dirname}/client/src/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/client/dist'
+    path: `${__dirname}/client/dist`,
   },
   module: {
     rules: [
@@ -12,10 +12,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 };
